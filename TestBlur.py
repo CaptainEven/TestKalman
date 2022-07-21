28c3c82f0c4745c0b965686b24213043b747e26e
Even <765305261@qq.com>
2022-07-21 17:09:08 +0800

    Refactor and comments.

--- TestBlur.py
+++ TestBlur.py
@@ -1,9 +1,14 @@
 # encoding=utf-8
 
-import cv2
-import numpy as np
+import os
 
+import cv2
+import matplotlib
+import numpy as np
 import scipy
+
+matplotlib.use('PS')
+import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
 
 
@@ -87,7 +92,7 @@ def anisotropic_gaussian_kernel_matlab(l,
     center = l / 2.0 - 0.5
     x, y = np.mgrid[-center:-center + l:1, -center:-center + l:1]
     pos = np.dstack((y, x))
-    k = scipy.stats.multivariate_normal.pdf(pos, mean=[0, 0], cov=cov)
+    k = multivariate_normal.pdf(pos, mean=[0, 0], cov=cov)
 
     k[k < scipy.finfo(float).eps * k.max()] = 0
 
@@ -146,3 +151,115 @@ def random_gaussian_kernel(l=21,
                                                   sig_min=sig_min,
                                                   sig_max=sig_max,
                                                   tensor=tensor)
+
+
+def plot_kernel(out_k_np, save_path, gt_k_np=None):
+    """
+    :param out_k_np:
+    :param save_path:
+    :param gt_k_np:
+    :return:
+    """
+    plt.clf()
+
+    if gt_k_np is None:
+        ax = plt.subplot(111)
+        im = ax.imshow(out_k_np, vmin=out_k_np.min(), vmax=out_k_np.max())
+        plt.colorbar(im, ax=ax)
+    else:
+        ax = plt.subplot(121)
+        im = ax.imshow(gt_k_np, vmin=gt_k_np.min(), vmax=gt_k_np.max())
+        plt.colorbar(im, ax=ax)
+        ax.set_title('GT Kernel')
+
+        ax = plt.subplot(122)
+        im = ax.imshow(out_k_np, vmin=gt_k_np.min(), vmax=gt_k_np.max())
+        plt.colorbar(im, ax=ax)
+        ax.set_title('Kernel PSNR: {:.2f}'
+                     .format(calculate_kernel_psnr(out_k_np, gt_k_np)))
+
+    plt.show()
+    plt.savefig(save_path)
+
+
+def draw_kernel(kernel_img, save_kernel_path, ratio=15):
+    """
+    :param kernel_img:
+    :param save_kernel_path:
+    :return:
+    """
+    kernel_img = cv2.normalize(kernel_img, None, 0, 255, cv2.NORM_MINMAX)
+    kernel_img = kernel_img.round()
+    kernel_img = kernel_img.astype(np.uint8)
+
+    k_h, k_w = kernel_img.shape
+    if ratio > 0:
+        ## ----- INTER_NEAREST
+        kernel_img = cv2.resize(kernel_img,
+                                (int(k_w * ratio), int(k_h * ratio)),
+                                cv2.INTER_AREA)
+
+    cv2.imwrite(save_kernel_path, kernel_img)
+    print("[Info]: {:s} saved.".format(save_kernel_path))
+
+
+def test_blur():
+    """
+    :return:
+    """
+
+    img_name = "b93aa54e08fb7d1b672d7bffb332bf73d96e942696dad2a2c67358d840bb5cbc.jpg"
+    img_dir = "e:/BKGImgs"
+    img_path = img_dir + "/" + img_name
+    if not os.path.isfile(img_path):
+        print("[Err]: invalid image path: {:s}".format(img_path))
+
+    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    h, w, c = img.shape
+    if max(w, h) > 1000:
+        img = cv2.resize(img, (int(w // 2), int(h // 2)), cv2.INTER_AREA)
+
+    for i in range(10):
+        ## ----- generate random blurring kernel
+        k_size = np.random.randint(3, 10)  # [3, 7]
+        kernel, sigma = random_gaussian_kernel(l=k_size,
+                                               sig_min=0.5,
+                                               sig_max=7,
+                                               rate_iso=0.2,
+                                               tensor=False)
+
+        ## ----- apply blurring
+        blurred_img = cv2.filter2D(img, -1, kernel)
+        ## -----
+
+        kernel_img = kernel.copy()
+        kernel_img = (kernel_img * 255.0).round()
+        kernel_img = kernel_img.astype(np.uint8)
+
+        # k_h, k_w = kernel_img.shape
+        # if kernel_img.shape[0] < 10:
+        #     kernel_img = cv2.resize(kernel_img, (k_w * 10, k_h * 10), cv2.INTER_AREA)
+        # elif 10 < kernel_img.shape[0] < 20:
+        #     kernel_img = cv2.resize(kernel_img, (k_w * 10, k_h * 5), cv2.INTER_AREA)
+        # elif 20 < kernel_img.shape[0] < 30:
+        #     kernel_img = cv2.resize(kernel_img, (k_w * 10, k_h * 3), cv2.INTER_AREA)
+
+        win_name = "blurring, kernel_size: {:d}".format(k_size)
+
+        # cv2.imshow(win_name, blurred_img)
+        # cv2.waitKey()
+
+        save_img_path = "./blurred_{:d}_{:d}.png".format(i, k_size)
+        cv2.imwrite(save_img_path, blurred_img)
+
+        # cv2.imshow("kernel size: {:d}".format(k_size), kernel_img)
+        save_kernel_path = "./kernel_{:d}_{:d}.png".format(i, k_size)
+        # plot_kernel(kernel_img, save_kernel_path)  ## PIL
+        draw_kernel(kernel_img, save_kernel_path)  ## OpenCV
+
+    print("Done.")
+
+
+if __name__ == "__main__":
+    test_blur()
+    print("Done.")
